6392a7f72b571300505750f0458c4df75ec26774
AndreyMSP <lanflex@mail.ru>
2026-06-06 22:32:56 +0300

    add new files

---
+++ src/ru/javarush/java/core/level17/task14/Car.java
@@ -0,0 +1,9 @@
+package ru.javarush.java.core.level17.task14;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Car extends Vehicle {
+    void beep() {
+        System.out.println("Машина сигналит: Бип-бип!");
+    }
+}
