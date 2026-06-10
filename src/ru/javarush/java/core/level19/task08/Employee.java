636d421075fea28723ddf0644dfb43fb35ec4fa9
AndreiMSP <lanflex@mail.ru>
2026-06-10 18:17:12 +0300

    add many files

---
+++ src/ru/javarush/java/core/level19/task08/Employee.java
@@ -0,0 +1,6 @@
+package ru.javarush.java.core.level19.task08;
+
+public abstract class Employee {
+    String name;
+    public abstract double getSalary();
+}
---
+++ src/ru/javarush/java/core/level19/task08/Manager.java
@@ -0,0 +1,6 @@
+package ru.javarush.java.core.level19.task08;
+
+public class Manager {
+    double baseSalary;
+
+}
